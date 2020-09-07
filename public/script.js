0d3692eeff9f5333863dbdfffe6fca077cb37a4a
vikas199 <mvikasraj123@gmail.com>
2020-09-07 15:24:28 -0500

    added video exchange b/w users

--- public/script.js
+++ public/script.js
@@ -1,20 +1,51 @@
 //gets the video and audio from the chrome
 
-
-const videoGrid = document.getElementById('video-grid')
+const socket = io("/")
+const videoGrid = document.getElementById("video-grid")
 const myVideo = document.createElement("video")
 myVideo.muted = true
 
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+})
+
 let myVideoStream
-navigator.mediaDevices     //check for available media devices
+navigator.mediaDevices //check for available media devices
   .getUserMedia({
     video: true,
-    audio: true,
-  }).then(stream => {
+    audio: false,
+  })
+  .then((stream) => {
     myVideoStream = stream
     addVideoStream(myVideo, stream)
+
+    peer.on("call", function (call) {
+      call.answer(stream) // Answer the call with an A/V stream.
+      const video = document.createElement("video")
+      call.on("stream", function (remoteStream) {
+        addVideoStream(video, remoteStream)
+      })
+    })
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream)
+    })
   })
 
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id)
+})
+
+const connectToNewUser = (userId, stream) => {
+  // console.log('new user', userId)
+  const call = peer.call(userId, stream)
+  const video = document.getElementById("video")
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream)
+  })
+}
 const addVideoStream = (video, stream) => {
   video.srcObject = stream
   video.addEventListener("loadedmetadata", () => {
